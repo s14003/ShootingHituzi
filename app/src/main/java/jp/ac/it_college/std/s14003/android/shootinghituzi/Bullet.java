980928bed29cae2ff757466fad4970da1a7a668e
s14003 <s14003@std.it-college.ac.jp>
2016-02-02 15:54:25 +0900

    縦スクロールと難易度調整以外完成。当たり判定がおかしいからそこの調整もする。

--- app/src/main/java/jp/ac/it_college/std/s14003/android/shootinghituzi/Bullet.java
+++ app/src/main/java/jp/ac/it_college/std/s14003/android/shootinghituzi/Bullet.java
@@ -1,5 +1,6 @@
 package jp.ac.it_college.std.s14003.android.shootinghituzi;
 
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Rect;
@@ -9,20 +10,36 @@ public class Bullet extends BaseObject {
     private static final float SIZE = 15f;
     public final float alignX;
     private final Paint paint = new Paint();
+    private final Bitmap bitmap;
+    private Rect rect;
 
-    Bullet(float alignX, Rect rect) {
+    Bullet(Bitmap bitmap,float alignX, Rect rect) {
+        this.bitmap = bitmap;
         this.alignX = alignX;
         yPosition = rect.centerY();
-        xPosotion = rect.centerX();
+        xPosition = rect.centerX();
+    }
+
+    @Override
+    public Type getType() {
+        return Type.Bullet;
+    }
+
+    @Override
+    public boolean isHit(BaseObject object) {
+        return object.getType() == Type.Missile && (calcDistance(this, object) < SIZE);
     }
 
     @Override
     public void draw(Canvas canvas) {
-        canvas.drawCircle(xPosotion,yPosition,SIZE,paint);
+        if (status == STATUS_NORMAL) {
+            canvas.drawCircle(xPosition, yPosition, SIZE, paint);
+        }
     }
 
     @Override
     public void move() {
-
+        yPosition -= 1 * MOVE_WEIGHT;
+        xPosition += alignX * MOVE_WEIGHT;
     }
 }
