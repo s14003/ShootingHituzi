ecd9b9f724bb0d5f6d18a5141e23f85fce4bd38d
s14003 <s14003@std.it-college.ac.jp>
2016-02-02 10:48:13 +0900

    ちょっと追加

--- app/src/main/java/jp/ac/it_college/std/s14003/android/shootinghituzi/Bullet.java
+++ app/src/main/java/jp/ac/it_college/std/s14003/android/shootinghituzi/Bullet.java
@@ -1,12 +1,14 @@
 package jp.ac.it_college.std.s14003.android.shootinghituzi;
 
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.graphics.Rect;
 
 
 public class Bullet extends BaseObject {
     private static final float SIZE = 15f;
     public final float alignX;
+    private final Paint paint = new Paint();
 
     Bullet(float alignX, Rect rect) {
         this.alignX = alignX;
@@ -16,7 +18,7 @@ public class Bullet extends BaseObject {
 
     @Override
     public void draw(Canvas canvas) {
-
+        canvas.drawCircle(xPosotion,yPosition,SIZE,paint);
     }
 
     @Override
