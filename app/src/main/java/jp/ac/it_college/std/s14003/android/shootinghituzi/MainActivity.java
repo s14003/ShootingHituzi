82eada543c7b59822fdc59832d7bf3e3d95ddabb
s14003 <s14003@std.it-college.ac.jp>
2016-02-05 13:43:24 +0900

    Result画面の作成、遷移を確認

--- app/src/main/java/jp/ac/it_college/std/s14003/android/shootinghituzi/MainActivity.java
+++ app/src/main/java/jp/ac/it_college/std/s14003/android/shootinghituzi/MainActivity.java
@@ -1,5 +1,6 @@
 package jp.ac.it_college.std.s14003.android.shootinghituzi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -19,6 +20,8 @@ public class MainActivity extends AppCompatActivity implements GameView.Callback
     public void onGameOver(long score) {
         gameView.stopDrawThread();
         Toast.makeText(this, "Game Over スコア" + score, Toast.LENGTH_LONG).show();
+        Intent it = new Intent(this, Result.class);
+        startActivity(it);
 
     }
 
