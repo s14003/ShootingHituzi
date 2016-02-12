6ce9a172d563625097f5da9aa0d93cf17a51becf
s14003 <s14003@std.it-college.ac.jp>
2016-02-12 09:07:18 +0900

    キャラクターを配置中

--- app/src/main/java/jp/ac/it_college/std/s14003/android/shootinghituzi/TapGame.java
+++ app/src/main/java/jp/ac/it_college/std/s14003/android/shootinghituzi/TapGame.java
@@ -2,25 +2,23 @@ package jp.ac.it_college.std.s14003.android.shootinghituzi;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 
 public class TapGame extends AppCompatActivity {
-    private TapCharacter tapCharacter;
-    private Canvas canvas;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tap);
+        drawGame();
+    }
+    private void drawGame() {
+        Bitmap inu = BitmapFactory.decodeResource(getResources(),R.drawable.hitujiinu);
+        ImageView iv = (ImageView)findViewById(R.id.imageView);
+        iv.setImageBitmap(inu);
 
-        int width = canvas.getWidth();
-        int height = canvas.getHeight();
-        if (tapCharacter == null) {
-            Bitmap inu = BitmapFactory.decodeResource(getResources(),R.drawable.hitujiinu);
-            tapCharacter = new TapCharacter(inu, width, height);
-        }
     }
 
 }
